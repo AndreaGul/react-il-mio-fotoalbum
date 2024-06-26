3d8bd2eaf3f1191c4913c97bce73b4826e83617e
AndreaGul <andreagulino99@gmail.com>
2024-06-26 17:31:42 +0200

    feat:validation photos

--- backend/routers/photo.js
+++ backend/routers/photo.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router= express.Router();
 const {store,show,index,update,destroy}= require("../controllers/photo");
 const upload = require('../middlewares/upload');
+const validator = require('../middlewares/validator');
+const {bodyData} = require('../validations/photos')
 
-router.post('/', upload.single('img') ,store);
+router.post('/', [upload.single('img'),validator(bodyData)] ,store);
 
 router.get('/:id',show)
 
 router.get('/',index);
 
-router.put('/:id',upload.single('img'),update)
+router.put('/:id',[upload.single('img'),validator(bodyData)],update)
 
 router.delete('/:id', destroy)
 
---
+++ backend/validations/photos.js
@@ -0,0 +1,84 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const bodyData = {
+  title: {
+    in: ["body"],
+    notEmpty: {
+      errorMessage: "Title è un campo obbligatorio.",
+      bail: true,
+    },
+    isString: {
+      errorMessage: "Title deve essere una stringa.",
+      bail: true,
+    },
+    isLength: {
+      errorMessage: "Title deve essere di almeno 3 caratteri",
+      options: { min: 3 },
+    },
+    trim: true,
+  },
+  description: {
+    in: ["body"],
+    notEmpty: {
+      errorMessage: "Description è un campo obbligatorio.",
+      bail: true,
+    },
+    isString: {
+      errorMessage: "Description deve essere una stringa.",
+      bail: true,
+    },
+  },
+  visible: {
+    in: ["body"],
+    isBoolean: {
+      errorMessage: "Visible deve essere un booleano.",
+    },
+    toBoolean: true,
+  },
+  img: {
+    custom: {
+      options: (value, { req }) => {
+        if (!req.file) {
+          throw new Error('Image file is required.');
+        }
+        // Altre validazioni personalizzate sull'immagine, se necessarie
+        return true;
+      },
+    },
+  },
+  categories:{
+    in:["body"],
+    notEmpty:{
+        errorMessage: 'le categorie sono un campo obbligatorio',
+        bail:true,
+    },
+    isArray:{
+        errorMessage:"le categorie devono essere un array",
+    },
+    custom: {
+        options: async (idCercati) => {
+    
+            const notIntegerId = idCercati.find(id => isNaN(parseInt(id)));
+            if (notIntegerId) {
+                throw new Error(`Uno o più ID non sono dei numeri interi.`);
+            }
+    
+            const categories = await prisma.category.findMany({
+                //gli id vengono passati come stringa quindi bisogna ritrasformarli in numeri
+                where: { id: { in: idCercati.map(id => parseInt(id)) } }
+            });
+    
+            if (tags.length !== idCercati.length) {
+                throw new Error(`Una o più categorie specificate non esistono`);
+            }
+    
+            return true;
+        }
+    }
+}
+};
+
+module.exports = {
+  bodyData,
+};
